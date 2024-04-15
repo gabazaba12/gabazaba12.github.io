e11a19e03c9ad195b320971f31d01613925baf78
gabazaba12 <107361813+gabazaba12@users.noreply.github.com>
2024-04-15 03:10:51 +0200

    sdfe

--- script.js
+++ script.js
@@ -39,35 +39,24 @@ let currentImageIndex = 0;
 
     showImage(currentImageIndex);
 
-// script.js
+const releaseDate = new Date(2025, 8, 10); 
 
-// script.js
-
-// Data premiery filmu (rok, miesiąc (liczone od 0), dzień, godzina, minuta, sekunda)
-const releaseDate = new Date(2025, 8, 10); // 10 września 2025 roku
-
-// Funkcja obliczająca pozostały czas do premiery filmu i aktualizująca odliczanie co sekundę
 function updateCountdown() {
-    const now = new Date(); // Aktualna data i czas
-    const difference = releaseDate - now; // Różnica między datami w milisekundach
+    const now = new Date(); 
+    const difference = releaseDate - now; 
 
     if (difference <= 0) {
-        // Jeśli premiera filmu już minęła
         document.getElementById('countdown').innerText = 'Premiera filmu już się odbyła!';
     } else {
-        // Obliczanie pozostałego czasu
         const days = Math.floor(difference / (1000 * 60 * 60 * 24));
         const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-        // Aktualizacja wyświetlanego odliczania
         document.getElementById('countdown').innerHTML = `Do premiery filmu pozostało: ${days} dni ${hours} godz. ${minutes} min ${seconds} sek`;
     }
 }
 
-// Wywołanie funkcji updateCountdown co sekundę
 setInterval(updateCountdown, 1000);
 
-// Wywołanie funkcji updateCountdown po załadowaniu strony
 updateCountdown();
